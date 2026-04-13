2adf581e5533c0a982e1455596d747c8454f9840
Om Shukla <omshukla271104@gmail.com>
2026-04-13 18:49:45 +0530

    Setting up for deployment

--- src/main/java/com/pblproject/dietrecc/controller/AuthController.java
+++ src/main/java/com/pblproject/dietrecc/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
--- src/main/java/com/pblproject/dietrecc/controller/DietController.java
+++ src/main/java/com/pblproject/dietrecc/controller/DietController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/diet")
 public class DietController {
